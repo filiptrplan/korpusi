e365134fff55d36fd330a8e494be0bff81d5610d
Filip Trplan <filip@trplan.si>
2023-10-18 01:51:37 +0200

    Basic displaying of first n and last n notes

--- web-app/app/components/OSMD.tsx
+++ web-app/app/components/OSMD.tsx
@@ -1,27 +1,142 @@
-import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
-import { useMemo, useRef } from "react";
+import {
+  GraphicalMusicSheet,
+  OpenSheetMusicDisplay,
+} from "opensheetmusicdisplay";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 
 export interface OSMDProps {
-  xml: string;
+  xml?: string;
+  displayFirstNNotes?: number;
+  displayLyrics?: boolean;
+  displayLastNNotes?: number;
 }
 
-export default function OSMD({ xml }: OSMDProps) {
-  const idContainer = useMemo(() => {
-    return "testniID";
-  }, []);
+export const OSMD: React.FC<OSMDProps> = ({
+  xml,
+  displayFirstNNotes,
+  displayLastNNotes,
+  displayLyrics = false,
+}) => {
   const ref = useRef(null);
+  const [originalGraphicSheet, setOriginalGraphicSheet] =
+    useState<GraphicalMusicSheet>();
   const osmd = useMemo(() => {
     if (ref.current === null) return null;
-    return new OpenSheetMusicDisplay(idContainer, { autoResize: false });
+    return new OpenSheetMusicDisplay(ref.current, {
+      autoResize: false,
+      drawingParameters: "compacttight",
+      drawCredits: false,
+      disableCursor: true,
+    });
   }, [ref.current]);
 
-  useMemo(() => {
+  const resetOSMD = async () => {
     if (osmd === null) return;
-    if (xml === "") return;
-    osmd.load(xml).then(() => {
+    if (xml === "" || !xml) return;
+    await osmd.load(xml);
+    if (ref.current) {
+      (ref.current as HTMLElement).innerHTML = "";
+    }
+  };
+
+  // These are done as functions and not in useEffect because we have to reset the OSMD object
+  // if we remove something and want to add it back
+  useEffect(() => {
+    if (osmd === null) return;
+    resetOSMD().then(() => {
+      osmdDisplayFirstNNotes(displayFirstNNotes);
+      osmdDisplayLyrics(displayLyrics);
+      osmdDisplayLastNNotes(displayLastNNotes);
       osmd.render();
     });
-  }, [osmd, xml]);
+  }, [osmd, xml, displayFirstNNotes, displayLyrics, displayLastNNotes]);
 
-  return <div ref={ref} id={idContainer}></div>;
-}
+  // Remove lyrics
+  const osmdDisplayLyrics = (_displayLyrics?: boolean) => {
+    if (osmd === null || _displayLyrics !== false) return;
+    osmd.GraphicSheet.MeasureList.forEach((measureList) => {
+      measureList.forEach((measure) => {
+        measure.staffEntries.forEach((staffEntry) => {
+          staffEntry.LyricsEntries = [];
+        });
+      });
+    });
+  };
+
+  // Display first n notes
+  const osmdDisplayFirstNNotes = (_displayFirstNNotes?: number) => {
+    if (osmd == null || !_displayFirstNNotes) return;
+    let noteCounter = 0;
+    let measureNumber = -1;
+    osmd.GraphicSheet.MeasureList.forEach((measureList) => {
+      measureList.forEach((measure) => {
+        measure.staffEntries.forEach((staffEntry) => {
+          staffEntry.graphicalVoiceEntries.forEach((voiceEntry) => {
+            if (voiceEntry.notes) {
+              const notes = voiceEntry.notes.filter(
+                (x) => !x.sourceNote.isRest()
+              );
+              noteCounter += notes.length;
+              if (noteCounter >= _displayFirstNNotes && measureNumber === -1) {
+                measureNumber = measure.MeasureNumber;
+              }
+              if (noteCounter > _displayFirstNNotes) {
+                notes.forEach((note) => {
+                  note.sourceNote.NoteheadColor = "#b6b6b6";
+                });
+              }
+            }
+          });
+        });
+      });
+    });
+
+    if (measureNumber !== -1) {
+      osmd.setOptions({
+        drawUpToMeasureNumber: measureNumber + 1,
+      });
+    }
+  };
+
+  const osmdDisplayLastNNotes = (_displayLastNNotes?: number) => {
+    if (osmd == null || !_displayLastNNotes) return;
+    let noteCounter = 0;
+    let measureNumber = -1;
+    const reversedMeasureList = [...osmd.GraphicSheet.MeasureList].reverse();
+    reversedMeasureList.forEach((measureList) => {
+      const reversedMeasureList = [...measureList].reverse();
+      reversedMeasureList.forEach((measure) => {
+        const reversedStaffEntries = [...measure.staffEntries].reverse();
+        reversedStaffEntries.forEach((staffEntry) => {
+          const reversedVoiceEntries = [
+            ...staffEntry.graphicalVoiceEntries,
+          ].reverse();
+          reversedVoiceEntries.forEach((voiceEntry) => {
+            if (voiceEntry.notes) {
+              const notes = voiceEntry.notes.filter(
+                (x) => !x.sourceNote.isRest()
+              );
+              noteCounter += notes.length;
+              if (noteCounter >= _displayLastNNotes && measureNumber === -1) {
+                measureNumber = measure.MeasureNumber;
+              }
+              if (noteCounter > _displayLastNNotes) {
+                notes.forEach((note) => {
+                  note.sourceNote.NoteheadColor = "#b6b6b6";
+                });
+              }
+            }
+          });
+        });
+      });
+    });
+
+    if (measureNumber !== -1) {
+      osmd.setOptions({
+        drawFromMeasureNumber: measureNumber + 1,
+      });
+    }
+  };
+
+  return <div ref={ref}></div>;
+};
--- web-app/app/routes/_index.tsx
+++ web-app/app/routes/_index.tsx
@@ -1,17 +1,110 @@
-import * as React from "react";
-import type { MetaFunction } from "@remix-run/node";
-import { Link as RemixLink } from "@remix-run/react";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-
-export const meta: MetaFunction = () => [{ title: "Mladinska glasba" }];
+import {
+  Card,
+  CardContent,
+  InputLabel,
+  Slider,
+  Stack,
+  TextField,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { OSMD } from "~/components/OSMD";
+import { FormControl } from "@mui/base";
 
 export default function Index() {
+  const [xml, setXml] = useState("");
+
+  const [firstNNotes, setFirstNNotes] = useState(8);
+  const [lastNNotes, setLastNNotes] = useState(8);
+
+  useEffect(() => {
+    fetch("/sample.xml").then(async (response) => {
+      const text = await response.text();
+      setXml(text);
+    });
+  }, []);
+
   return (
     <>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Pozdravljeni na spletni strani Mladinske glasbe!
-      </Typography>
+      <Stack spacing={2}>
+        <Card variant="outlined">
+          <CardContent>
+            <FormControl>
+              <InputLabel htmlFor="first-n-notes">
+                Prikaži prvih N not
+              </InputLabel>
+              <Stack
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                id="first-n-notes"
+              >
+                <TextField
+                  variant="standard"
+                  size="small"
+                  sx={{
+                    width: "3rem",
+                  }}
+                  type="number"
+                  value={firstNNotes}
+                  onChange={(e) => {
+                    setFirstNNotes(parseInt(e.target.value));
+                  }}
+                />
+                <Slider
+                  min={3}
+                  max={20}
+                  value={firstNNotes ?? 0}
+                  marks={true}
+                  step={1}
+                  onChange={(e, num) => {
+                    setFirstNNotes(num as number);
+                  }}
+                />
+              </Stack>
+            </FormControl>
+            <OSMD xml={xml} displayFirstNNotes={firstNNotes} />
+          </CardContent>
+        </Card>
+        <Card variant="outlined">
+          <CardContent>
+            <FormControl>
+              <InputLabel htmlFor="last-n-notes">
+                Prikaži zadnjih N not
+              </InputLabel>
+              <Stack
+                direction="row"
+                spacing={2}
+                alignItems="center"
+                id="last-n-notes"
+              >
+                <TextField
+                  variant="standard"
+                  size="small"
+                  sx={{
+                    width: "3rem",
+                  }}
+                  type="number"
+                  value={lastNNotes}
+                  onChange={(e) => {
+                    setLastNNotes(parseInt(e.target.value));
+                  }}
+                />
+                <Slider
+                  min={3}
+                  max={20}
+                  value={lastNNotes ?? 0}
+                  marks={true}
+                  step={1}
+                  onChange={(e, num) => {
+                    setLastNNotes(num as number);
+                  }}
+                />
+              </Stack>
+            </FormControl>
+            <OSMD xml={xml} displayLastNNotes={lastNNotes} />
+          </CardContent>
+        </Card>
+      </Stack>
     </>
   );
 }
