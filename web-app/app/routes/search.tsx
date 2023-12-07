7088b4459038e53a3b7cf2e93a8fdf4a39a70360
Filip Trplan <filip@trplan.si>
2023-12-07 18:11:03 +0100

    separate component for results

--- web-app/app/routes/search.tsx
+++ web-app/app/routes/search.tsx
@@ -44,6 +44,7 @@ import { MelodicNgramSearch } from "./search/MelodicNgramSearch";
 import notes from "./search/notes.json";
 import { CorpusSelect } from "./search/CorpusSelect";
 import { FilterGroupCollapse } from "./search/FilterGroupCollapse";
+import { ResultList } from "./search/ResultList";
 
 export let handle = {
   i18n: "search",
@@ -331,17 +332,6 @@ export default function Search() {
     localStorage.setItem("searchParams", JSON.stringify(params));
   }, [params]);
 
-  const resultComponents = data.map((song) => {
-    return (
-      <ResultRow
-        songHit={song}
-        key={song._id}
-        corpusOptions={availableCorpuses}
-      />
-    );
-  });
-
-  const searching = navigation.state !== "idle";
   const submit = useSubmit();
 
   const resetFields = () => {
@@ -470,105 +460,11 @@ export default function Search() {
           </Stack>
         </Stack>
       </Form>
-      <Stack
-        direction={"row"}
-        sx={{
-          width: "100%",
-          my: 1,
-        }}
-        alignContent={"center"}
-        alignItems={"center"}
-      >
-        <Typography
-          variant="caption"
-          sx={{
-            whiteSpace: "nowrap",
-          }}
-        >
-          {t("results", { count: pagination.totalHits })}
-        </Typography>
-        <Divider sx={{ mx: 1, flexGrow: 1 }} />
-        <Button variant="text" size="small">
-          Izberi dela
-        </Button>
-      </Stack>
-      <Box
-        sx={{
-          position: "relative",
-        }}
-      >
-        <CircularProgress
-          sx={{
-            opacity: searching ? 1 : 0,
-            zIndex: 100,
-            position: "absolute",
-            left: "50%",
-            top: "50%",
-            marginLeft: "-1rem",
-            marginTop: "-1rem",
-            transition: "opacity 0.35s ease",
-          }}
-        />
-        <Stack
-          spacing={1}
-          sx={{
-            mt: 1,
-            opacity: searching ? 0.5 : 1,
-            transition: "opacity 0.35s ease",
-            minHeight: "5rem",
-          }}
-        >
-          {resultComponents}
-        </Stack>
-      </Box>
-      <Stack
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        gap={2}
-        sx={{
-          mt: 2,
-        }}
-      >
-        <FormControl size="small">
-          <InputLabel id="page-size-label">Št. na stran</InputLabel>
-          <Select
-            labelId="page-size-label"
-            defaultValue={10}
-            sx={{
-              minWidth: "7rem",
-            }}
-            size="small"
-            label="Št. na stran"
-            value={pagination.pageSize}
-            onChange={(e) => {
-              const link = new URLSearchParams(params);
-              const pageSize = parseInt(e.target.value as string);
-              link.set("pageSize", e.target.value.toString());
-              if (pageSize * (pagination.current - 1) > pagination.totalHits) {
-                link.set(
-                  "page",
-                  Math.ceil(pagination.totalHits / pageSize).toString()
-                );
-              }
-              navigate(`?${link.toString()}`);
-            }}
-          >
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={50}>50</MenuItem>
-          </Select>
-        </FormControl>
-        <Pagination
-          count={pagination.total}
-          page={pagination.current}
-          onChange={(e, page) => {
-            const link = new URLSearchParams(params);
-            link.set("page", page ? page.toString() : "1");
-            navigate(`?${link.toString()}`);
-          }}
-        />
-      </Stack>
+      <ResultList
+        pagination={pagination}
+        songHits={data}
+        availableCorpuses={availableCorpuses}
+      />
     </div>
   );
 }
