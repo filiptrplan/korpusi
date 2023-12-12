274b77cb1eda7ed24ad4645a2c09740b6701d801
Filip Trplan <filip@trplan.si>
2023-12-12 21:30:05 +0100

    add documentation to the song data type

--- web-app/app/src/DataTypes.ts
+++ web-app/app/src/DataTypes.ts
@@ -8,24 +8,68 @@ export interface SongResult extends Record<string, any> {
     copyright: string;
   };
   ambitus: {
+    /**
+     * Number of semitones between the highest and lowest note
+     */
     ambitus_semitones: number;
+    /**
+     * Highest note in MIDI pitch value
+     */
     max_note: number;
+    /**
+     * Lowest note in MIDI pitch value
+     */
     min_note: number;
   };
   contour: {
+    /**
+     * Indexes of the start of each measure
+     */
     measure_starts: number[];
+    /**
+     * Relative pitches, only UP, DOWN, SAME, e.g. "U U U D S"
+     */
     melodic_contour_string: string;
+    /**
+     * The pitch values are relative to the previous note.
+     */
     melodic_contour_string_relative: string;
+    /**
+     * The pitch values are absolute and are MIDI pitch values.
+     */
     melodic_contour_string_absolute: string;
   };
   key: {
+    /**
+     * The top alternative most likely keys
+     */
     alternative_keys: string[];
+    /**
+     * Most likely key, e.g. "C#"
+     */
     most_certain_key: string;
   };
   rhythm: {
+    /**
+     * Used for searching for songs with the same rhythm.
+     * E.g. "1/1 1/2 1/2 1/1 1/4"
+     */
     rhythm_string: string;
+    /**
+     * Indexes of the start of each measure
+     */
     measure_starts: number[];
   };
+  /**
+   * Tempo in BPM
+   */
   tempo: number;
+  /**
+   * Time signature, e.g. "4/4"
+   */
   time_signature: string;
+  /**
+   * Original musicXML file
+   */
+  original_file: string;
 }
