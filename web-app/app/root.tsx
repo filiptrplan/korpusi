30c67b035674b411dc46cd9f41a9d3aadad6d66b
Filip Trplan <filip@trplan.si>
2023-12-14 18:39:33 +0100

    add title and componentsjson to root

--- web-app/app/root.tsx
+++ web-app/app/root.tsx
@@ -9,6 +9,7 @@ import {
   useRouteError,
   isRouteErrorResponse,
   useLoaderData,
+  MetaFunction,
 } from "@remix-run/react";
 import { withEmotionCache } from "@emotion/react";
 import { unstable_useEnhancedEffect as useEnhancedEffect } from "@mui/material";
@@ -31,7 +32,11 @@ export let handle = {
   // will need to load. This key can be a single string or an array of strings.
   // TIP: In most cases, you should set this to your defaultNS from your i18n config
   // or if you did not set one, set it to the i18next default namespace "translation"
-  i18n: "common",
+  i18n: ["common", "components"],
+};
+
+export const meta: MetaFunction = () => {
+  return [{ title: "Mladinska glasba" }];
 };
 
 interface DocumentProps {
