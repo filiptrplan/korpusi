48b687f434743526bcb924febcddcb4b34eca43f
Filip Trplan <filip@trplan.si>
2024-03-19 19:37:23 +0100

    add option to  merge existing mapping to upload.py

--- pipeline/upload.py
+++ pipeline/upload.py
@@ -25,7 +25,11 @@ def calculate_hash(json_str: str):
     """Calculates the hash of the original file from the JSON string."""
     obj = json.loads(json_str)
     m = hashlib.sha256()
-    orig_file = obj["original_file"]
+    print(obj)
+    if "original_file" not in obj:
+        orig_file = obj["filename"]
+    else:
+        orig_file = obj["original_file"]
     m.update(orig_file.encode("utf-8"))
     return m.hexdigest()
 
@@ -37,6 +41,21 @@ def index_document(json_str: str, index: str):
     except json.JSONDecodeError as e:
         raise e
     client.index(index=index, document=json_str, id=calculate_hash(json_str))
+
+
+def merge(source, destination):
+    """
+    Deep merge two dictionaries. https://stackoverflow.com/questions/20656135/python-deep-merge-dictionary-data
+    """
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge(value, node)
+        else:
+            destination[key] = value
+
+    return destination
 
 
 @app.command()
@@ -64,12 +83,17 @@ def upload(
     delete_index: Annotated[
         bool, typer.Option(help="Whether to delete the index before uploading.")
     ] = False,
+    merge_mapping: Annotated[
+        bool, typer.Option(help="Whether to merge the mapping with the existing one.")
+    ] = False,
 ):
     """Uploads JSON files to the ElasticSearch database."""
     if json_file is not None and json_dir is not None:
         raise typer.BadParameter("Cannot specify both json_file and json_dir")
+
     if json_file is None and json_dir is None:
         raise typer.BadParameter("Must specify either json_file or json_dir")
+
     if mapping_file is None:
         if json_file is not None:
             mapping_file = os.path.join(os.path.dirname(json_file), "mapping.json")
@@ -85,8 +109,19 @@ def upload(
     if delete_index is True:
         client.options(ignore_status=404).indices.delete(index=index)
     client.options(ignore_status=400).indices.create(index=index)
+
+    merged_mapping = mapping
+    # print(merged_mapping)
+
+    if merge_mapping is True:
+        existing_mapping = client.indices.get_mapping(index=index)
+        existing_mapping_dict = existing_mapping[index]["mappings"]
+        merged_mapping = merge(mapping, existing_mapping_dict)
+
+    print(json.dumps(merged_mapping, indent=4))
+
     client.indices.put_mapping(
-        index=index, properties=mapping["properties"]
+        index=index, properties=merged_mapping["properties"]
     )  # this is so we don't ignore 400 errors on mapping syntax
 
     if json_file is not None:
